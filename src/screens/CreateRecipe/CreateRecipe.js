dc9785bea41120ef2fa53ce15fd172ac8db844e4
Sama Mansour <samamansour306@gmail.com>
2023-08-20 21:10:09 +0300

    connect to firestore

--- src/screens/CreateRecipe/CreateRecipe.js
+++ src/screens/CreateRecipe/CreateRecipe.js
@@ -8,7 +8,31 @@ import {
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { categories } from "../../data/dataArrays";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALY_L5QAVWGS_fx4oYJ8b3Fg-ni8sIELk",
+  authDomain: "recipes-app-6538e.firebaseapp.com",
+  databaseURL: "https://recipes-app-6538e-default-rtdb.firebaseio.com",
+  projectId: "recipes-app-6538e",
+  storageBucket: "recipes-app-6538e.appspot.com",
+  messagingSenderId: "943409972016",
+  appId: "1:943409972016:web:a0ab10c3f46f8b245b3ef5",
+  measurementId: "G-RJWRPB172G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default function CreateRecipeScreen(props) {
   const { navigation } = props;
 
@@ -18,7 +42,7 @@ export default function CreateRecipeScreen(props) {
   const [category, setCategory] = useState("");
   const [ingredients, setIngredients] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const newRecipe = {
       title: recipeTitle,
       description,
@@ -28,8 +52,17 @@ export default function CreateRecipeScreen(props) {
         .split(",")
         .map((ingredient) => ingredient.trim()),
     };
-    console.log(newRecipe);
+
+    const db = getFirestore(app);
+
+    try {
+      const docRef = await addDoc(collection(db, "recipes"), newRecipe);
+      console.log("Recipe added with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding recipe: ", error);
+    }
   };
+
 
   return (
     <ScrollView style={styles.container}>

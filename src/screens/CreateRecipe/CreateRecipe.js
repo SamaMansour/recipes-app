bfb13c25947060b6b7e84842263c87dc982550ff
Sama Mansour <samamansour306@gmail.com>
2023-08-20 12:48:03 +0300

    Fix Craete Recipe Screen Add styling and picker for categories

--- src/screens/CreateRecipe/CreateRecipe.js
+++ src/screens/CreateRecipe/CreateRecipe.js
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
 import {
   ScrollView,
-  Text,
   View,
-  Image,
-  Dimensions,
   TextInput,
   Button,
+  StyleSheet,
 } from "react-native";
-import styles from "./styles";
-import BackButton from "../../components/BackButton/BackButton";
-
-const { width: viewportWidth } = Dimensions.get("window");
+import { Picker } from "@react-native-picker/picker";
 
 export default function CreateRecipeScreen(props) {
   const { navigation } = props;
@@ -32,45 +27,79 @@ export default function CreateRecipeScreen(props) {
         .split(",")
         .map((ingredient) => ingredient.trim()),
     };
-    // TODO: Store the recipe or do something else with it
     console.log(newRecipe);
   };
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.infoRecipeContainer}>
+      <View style={styles.formGroup}>
         <TextInput
           placeholder="Recipe Title"
           style={styles.input}
           value={recipeTitle}
           onChangeText={setRecipeTitle}
         />
+      </View>
+      <View style={styles.formGroup}>
         <TextInput
           placeholder="Description"
           style={styles.input}
           value={description}
           onChangeText={setDescription}
         />
+      </View>
+      <View style={styles.formGroup}>
         <TextInput
           placeholder="Time (e.g., '15 minutes')"
           style={styles.input}
           value={time}
           onChangeText={setTime}
         />
-        <TextInput
-          placeholder="Category (e.g., 'Dessert')"
-          style={styles.input}
-          value={category}
-          onChangeText={setCategory}
-        />
+      </View>
+      <View style={styles.formGroup}>
+        <Picker
+          selectedValue={category}
+          style={styles.picker}
+          onValueChange={(itemValue) => setCategory(itemValue)}
+        >
+          <Picker.Item label="Dessert" value="dessert" />
+          <Picker.Item label="Main Course" value="mainCourse" />
+          {/* Add more categories as needed */}
+        </Picker>
+      </View>
+      <View style={styles.formGroup}>
         <TextInput
           placeholder="Ingredients (comma separated)"
           style={styles.input}
           value={ingredients}
           onChangeText={setIngredients}
         />
+      </View>
+      <View style={styles.formGroup}>
         <Button title="Submit Recipe" onPress={handleSubmit} />
       </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "#f4f4f4",
+  },
+  formGroup: {
+    marginBottom: 15,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "#fff",
+    elevation: 1,
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 10,
+  },
+  picker: {
+    height: 40,
+    width: "100%",
+  },
+});
